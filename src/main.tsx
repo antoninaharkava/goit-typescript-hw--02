bb0c296420731073dbfdafd848ec3ff51d0ddf74
antonina <tonya.garkava@gmail.com>
2024-11-26 20:15:36 +0100

    check commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "modern-normalize";
+import "modern-normalize/modern-normalize.css";
+
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
